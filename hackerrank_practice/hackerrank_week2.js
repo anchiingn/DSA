7a47f1753268f6457cba3a7a1ce0ecb37ce9b32c
Anchi <anchi.n248@gmail.com>
2024-02-21 22:53:07 -0600

    three more problems in week 2

---
+++ hackerrank_practice/hackerrank_week2.js
@@ -0,0 +1,60 @@
+function lonelyinteger(a) {
+    // Write your code here
+    let result = 0
+    for (let i = 0; i < a.length; i++) {
+        result ^= a[i]
+    }
+    return result
+
+}
+
+// console.log(lonelyinteger([0,0,1,2,1]))
+
+
+
+function gradingStudents(grades) {
+    // Write your code here
+
+    for (let i = 0; i < grades.length; i++) {
+        
+        if (grades[i] < 38) {
+            grades[i] = grades[i]
+        }
+        if(grades[i] >= 38) {
+            let mulBy5 = Math.ceil(grades[i]/5)*5
+            if (mulBy5 - grades[i] < 3) {
+                grades[i] = mulBy5
+            }
+        }
+    }
+    return grades
+
+}
+
+// console.log(gradingStudents([73,67,38,33]))
+
+
+function marsExploration(s) {
+    let sos = 'SOS';
+    let eachLetter = s.split('');
+    let count =0
+    for (let i = 0; i < eachLetter.length; i++) {
+        console.log(sos[i%2])
+        if (eachLetter[i] !== sos[i%3]) count++
+    }
+    return count
+}
+
+// console.log(marsExploration('SOSSPSSQSSOR'))
+
+function pangrams(s) {
+    // Write your code here
+    const alphabet = 'abcdefghijklmnopqrstuvwyz'
+    const lowercase = s.toLowerCase()
+    const eachLetter = lowercase.split('')
+    for (let letter of eachLetter ) {
+        console.log(letter)
+    }
+}
+
+console.log(pangrams('We promptly judged antique ivory buckles for the next prize'))
