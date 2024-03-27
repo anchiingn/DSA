1bc19887a9626dc91e785dd2d1deac56825d400b
Anchi <anchi.n248@gmail.com>
2024-03-27 14:50:43 -0500

    dsa

--- leetcode_practice/p1.js
+++ leetcode_practice/p1.js
@@ -10,4 +10,19 @@ var removeElement = function(nums, val) {
 };
 
 
-console.log(removeElement([0,1,2,2,3,0,4,2],2))
+// console.log(removeElement([0,1,2,2,3,0,4,2],2))
+
+
+
+var removeDuplicates = function(nums) {
+    let k = 0;  
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[k] !== nums[i]) {
+            k++;
+            nums[k] = nums[i]
+        }
+    }
+    return k +1;
+};
+
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
