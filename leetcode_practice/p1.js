6c9fa83473fb8ec5126fb8c85952fb72d55ceab2
Anchi <anchi.n248@gmail.com>
2024-03-27 14:33:46 -0500

    dsa

--- leetcode_practice/p1.js
+++ leetcode_practice/p1.js
@@ -1,3 +1,13 @@
 var removeElement = function(nums, val) {
-    
+    let k = 0;  //Innititally the track of non-val number
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) { // check if the first element of nums is the same as val
+            nums[k] = nums[i]  // if not then change the inital number to the non-val number
+            k++; //increase the track by one, to change the next number
+        }
+    }
+    return k
 };
+
+
+console.log(removeElement([0,1,2,2,3,0,4,2],2))
