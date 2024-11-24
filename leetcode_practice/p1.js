c21778e928bb9286536fe93b66ef530e439b9bef
Anchi <anchi.n248@gmail.com>
2024-11-23 20:35:43 -0600

    update

--- leetcode_practice/p1.js
+++ leetcode_practice/p1.js
@@ -242,7 +242,7 @@ var isAnagram = function(word1, word2) {
     return true
 };
 
-console.log(isAnagram("anagram","nagaram"))
+// console.log(isAnagram("anagram","nagaram"))
 
 
 var removeAnagrams = function(words) {
